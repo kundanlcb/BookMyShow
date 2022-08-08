6f4f471ce67c42729dcc6bba534f1149b546d050
kundankumarroy <kundan.roy@telekom-digital.com>
2022-08-08 09:55:36 +0530

    first commit

---
+++ src/main/java/com/bootninza/movieticketbooking/controller/MovieController.java
@@ -0,0 +1,12 @@
+package com.bootninza.movieticketbooking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+
+
+
+}
